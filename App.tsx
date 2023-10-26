8ac643fa6919b8cd2802d1574d55d6c3171d5bfd
Saba Pipia <pipiasaba788@gmail.com>
2023-10-26 18:03:49 +0400

    last commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import {
+  BarCodeScanner,
+  BarCodeScannerResult,
+  PermissionResponse,
+} from "expo-barcode-scanner";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<
+    PermissionResponse | null | boolean
+  >(null);
+  const [scanned, setScanned] = useState<boolean>(false);
+  const [scannedData, setScannedData] = useState<string>("");
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = (result: BarCodeScannerResult) => {
+    const { data } = result;
+    setScanned(true);
+    setScannedData(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={styles.cameraContainer}
+      />
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
+      <View>
+        {hasPermission === null ? (
+          <Text>Requesting for camera permission</Text>
+        ) : null}
+        <View style={styles.scannedData}>
+          <Text style={{ fontSize: 30 }}>&darr; Scanned Data &darr; </Text>
+          <Text style={{ fontSize: 30, color: "red" }}>{scannedData}</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +60,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+  },
+  cameraContainer: {
+    flex: 0.5,
+  },
+  scannedData: {
+    marginTop: 10,
+    alignItems: "center",
   },
 });
